f59ff5c6186511c384cd0d1dc5aeb6d5a7203b96
Tunde Idahor <bidahor13@wou.edu>
2016-09-01 12:51:10 -0700

    Sanner function

--- Solution.java
+++ Solution.java
@@ -18,27 +18,27 @@ import java.util.regex.*;
 
 public class Solution {
 
-    static int solveMeFirst(int a, int b) {
-        int sum = a + b ;
-        return sum;
-    }
+//    static int solveMeFirst(int a, int b) {
+//        int sum = a + b ;
+//        return sum;
+//    }
 
 
     /**
      * Warm-up
-     * 
+     *
      * @param args
      */
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a;
-        a = in.nextInt();
-        int b;
-        b = in.nextInt();
-        int sum;
-        sum = solveMeFirst(a, b);
-        System.out.println(sum);
-    }
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int a;
+//        a = in.nextInt();
+//        int b;
+//        b = in.nextInt();
+//        int sum;
+//        sum = solveMeFirst(a, b);
+//        System.out.println(sum);
+//    }
 
 
     /**
@@ -46,20 +46,71 @@ public class Solution {
      *
      * @param args
      */
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the size of your array: ");
-        int n = in.nextInt();
-        System.out.println("Now Enter the values into each array location: ");
-        int arr[] = new int[n];
-        int sum = 0;
-        for(int arr_i=0; arr_i < n; arr_i++) {
-            arr[arr_i] = in.nextInt();
-            sum +=arr[arr_i];
-        }
-            System.out.println("Number of objects in the list: "+ arr.length);
-            System.out.print("Total sum: "+ sum);
-    }
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter the size of your array: ");
+//        int n = in.nextInt();
+//        System.out.println("Now Enter the values into each array location: ");
+//        int arr[] = new int[n];
+//        int sum = 0;
+//        for(int arr_i=0; arr_i < n; arr_i++) {
+//            arr[arr_i] = in.nextInt();
+//            sum +=arr[arr_i];
+//        }
+//            System.out.println("Number of objects in the list: "+ arr.length);
+//            System.out.print("Total sum: "+ sum);
+//    }
 
+
+    /**
+     * TITLE: NEXT LINE OF CODE ON HACKER RANK
+     */
+
+    public static void main(String[] args) {
+
+
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter you First name: ");
+            String fName = input.next();
+            if (fName.matches("^-?\\d+$")) {
+                System.out.println("Invalid input data at First Name... ");
+
+            }
+
+            System.out.println("Enter you Last name: ");
+            String lName = input.next();
+            if ((lName.matches("^-?\\d+$") )) {
+                System.out.println("Invalid input data at Last Name... ");
+                return;
+            }
+
+            System.out.println("Enter your Age: ");
+            int age = input.nextInt();
+            if((age == (int)age)){
+
+                /**
+                 * Output to the Screen
+                 */
+                System.out.println("**********Bio Data below***************");
+                System.out.print(fName.trim() + " " + lName.trim());
+                System.out.println("\n Age: " + age);
+            }
+
+        } catch (InputMismatchException err) {
+            System.out.println(" Try Catch result: ==== Invalid data! Check your input");
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
 
